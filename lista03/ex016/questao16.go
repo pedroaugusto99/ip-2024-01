a2eca4c9b179143776cf8b6acf257df7a5e3f8b5
pedroaugusto99 <166179411+pedroaugusto99@users.noreply.github.com>
2024-05-03 14:52:15 -0300

    Create questao16.go

---
+++ lista03/ex016/questao16.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "fmt"
+  "sort"
+)
+
+func main() {
+  var N, K int
+  fmt.Scan(&N, &K)
+
+  horarios := make([]int, N)
+  for i := 0; i < N; i++ {
+    fmt.Scan(&horarios[i])
+  }
+  
+  indices := make(map[int]int)
+  for i, tempo := range horarios {
+    indices[tempo] = i + 1
+  }
+
+  
+  presentes := 0
+  for _, tempo := range horarios {
+    if tempo <= 0 {
+      presentes++
+    }
+  }
+
+  
+  if presentes < K {
+    fmt.Println("SIM")
+  } else {
+    fmt.Println("NÃO")
+    
+    sort.Ints(horarios)
+    //sort.Slice(horarios, func(i, j int) bool {
+      return horarios[j] > horarios[i]
+    })
+
+    for _, tempo := range horarios {
+      if tempo <= 0 {
+        fmt.Printf("%d\n", indices[tempo])
+      }
+      }
+    }
+    fmt.Println()
+  }
+
