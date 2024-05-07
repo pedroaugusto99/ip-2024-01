877ff8759c79271af336225f653588918e87c3f4
pedroaugusto99 <166179411+pedroaugusto99@users.noreply.github.com>
2024-05-07 12:25:24 -0300

    Create questao22.go

---
+++ lista03/ex022/questao22.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "fmt"
+)
+
+func encontrarMaiorNumero(numero string, d int) string {
+  n := len(numero)
+  if d >= n {
+    return numero
+  }
+
+  digitos := make([]byte, 0, n)
+  aRemover := n - d
+
+  for i := 0; i < n; i++ {
+    digitoAtual := numero[i]
+
+    for len(digitos) > 0 && digitos[len(digitos)-1] < digitoAtual && aRemover > 0 {
+      digitos =digitos[:len(digitos)-1]
+      aRemover--
+    }
+
+    digitos = append(digitos, digitoAtual)
+  }
+
+  return string(digitos[:d])
+}
+
+func main() {
+    var resultados []string
+  var n, d int
+  var numero string
+
+  for {
+    fmt.Scanf("%d %d", &n, &d)
+    if n == 0 && d == 0 {
+      break
+    }
+    fmt.Scan(&numero)
+
+    maiorNumero := encontrarMaiorNumero(numero, d)
+    resultados = append(resultados,maiorNumero)
+ 
+  }
+  for _, value := range resultados {
+    fmt.Println(value)
+  }
+}
